1cb85344f13aa1547e3d2b537f953a5073e00d3c
Renan Leonello Castro <renanleonellocastro@gmail.com>
2020-09-10 17:10:11 -0300

    Changes to upload correctly the automatedweb py_module to pip

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A tool to help in web requests such as POST and GET. It parses the html using jquery selectors as well.
 
 To use this tool, it is necessary to install the following packages:
- - Python 2
+ - Python 3
  - Python Requests
  - Python PyQuery
  - Python Urllib3
--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup
+import setuptools
 
-setup(
+#README as long_descriptions
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
     name='automatedweb',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/renanleonellocastro/automatedweb.git',
     license='MIT License',
     author='Renan Leonello Castro',
     author_email='renanleonellocastro@gmail.com',
     keywords='automatedweb web automated post get requests rest restfull',
-    description=u'A tool to make it easy to communicate with web systems writting and reading data from them',
+    description='A tool to make it easy to communicate with web systems writting and reading data from them',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    py_modules=["automatedweb"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         "requests>=2.22.0",
         "pyquery>=1.4.0",
